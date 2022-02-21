bbff1d6510b16612f1c1b44b905ebaf104c04e20
unknown <amir.bajraktarevic@propeller.ba>
2022-02-21 15:17:45 +0100

    Created Title component

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="text-center mt-5 mb-5 px-5">
+      <span>Din Psykolog Coding challenge</span>
+    </div>
+  );
+};
+
+export default Title;
